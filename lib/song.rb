e5799e177d7cf81a0792c3a1d15c68bd63397408
Brian DiRito <bdirito@gmail.com>
2018-09-20 16:13:11 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+require "pry"
+
+# count each_with
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres.each_with_object({})  do |index, hash|
+      hash[index] = @@genres.count(index)
+    end
+  end
+
+  def self.artist_count
+    @@artists.each_with_object({})  do |index, hash|
+      hash[index] = @@artists.count(index)
+    end
+  end
+end
